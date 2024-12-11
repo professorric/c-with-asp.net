34bffe872c602b389e30c686705199140cdc91b3
professorric <93748936+professorric@users.noreply.github.com>
2024-12-11 19:42:50 +0800

    Create Door.cs

---
+++ 5 Door Challenge/Door.cs
@@ -0,0 +1,8 @@
+namespace _20doorchallenge.Models
+{
+    public class Door
+    {
+        public bool IsOpen { get; set; } // Represents whether the door is open or closed
+        public string State { get; set; } = "close"; // "open" or "closed"
+    }
+}
